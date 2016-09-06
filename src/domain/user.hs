a8101563d678298d6ae6ae0f284145b9a23d7758
Viktor Befort <v.befort@googlemail.com>
2016-09-06 23:15:20 +0200

    erweitere user um acid-state

--- src/domain/user.hs
+++ src/domain/user.hs
@@ -1,11 +1,26 @@
-{-# LANGUAGE DeriveDataTypeable, GeneralizedNewtypeDeriving,
-    TemplateHaskell #-}
+{-# LANGUAGE CPP, DeriveDataTypeable, FlexibleContexts,
+  GeneralizedNewtypeDeriving, MultiParamTypeClasses,
+  TemplateHaskell, TypeFamilies, RecordWildCards #-}
 
 module Domain.User where
 
 import Prelude hiding (head)
 
-import Data.Data (Data, Typeable)
+import Control.Applicative  ( (<$>) )
+import Control.Exception    ( bracket )
+import Control.Monad        ( msum )
+import Control.Monad.Reader ( ask )
+import Control.Monad.State  ( get, put )
+import Data.Data            ( Data, Typeable )
+import Happstack.Server     ( Response, ServerPart, dir
+                            , nullDir, nullConf, ok
+                            , simpleHTTP, toResponse )
+import Data.Acid            ( AcidState, Query, Update
+                            , makeAcidic, openLocalState )
+import Data.Acid.Advanced   ( query', update' )
+import Data.Acid.Local      ( createCheckpointAndClose )
+import Data.SafeCopy        ( base, deriveSafeCopy )
+
 import Web.Routes ( PathInfo(..))
 import Web.Routes.TH  (derivePathInfo)
 
@@ -15,5 +30,23 @@ newtype UserId = UserId { unUserId :: Int }
 data Sitemap
   = Home
   | User UserId
-  
 $(derivePathInfo ''Sitemap)
+
+data UserState = UserState { name :: String }
+    deriving (Eq, Ord, Read, Show, Data, Typeable)
+$(deriveSafeCopy 0 'base ''UserState)
+
+initialUserState :: UserState
+initialUserState = UserState ""
+
+setName :: String -> Update UserState String
+setName n =
+    do c@UserState{..} <- get
+       let newName = name
+       put $ c { name = newName }
+       return newName
+
+peekName :: Query UserState String
+peekName = name <$> ask
+
+$(makeAcidic ''UserState ['setName, 'peekName])
