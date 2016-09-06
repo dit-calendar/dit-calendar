7efe1f83b445ac79d1b3255fd4fcdba20162a3f2
Viktor Befort <v.befort@googlemail.com>
2016-09-06 19:38:17 +0200

    move route-functions to new file

--- src/AppMain.hs
+++ src/AppMain.hs
@@ -2,39 +2,16 @@
 module Main where
 
 import Domain.User
+import Route.User
 
 import Prelude                 hiding (head)
 
 import Control.Monad           (msum)
-import Data.Data               (Data, Typeable)
-import Data.Monoid             (mconcat)
 import Data.Text
 import Happstack.Server
     ( Response, ServerPartT, ok, toResponse, simpleHTTP
     , nullConf, seeOther, dir, notFound, seeOther)
-import Web.Routes
-    ( PathInfo(..), RouteT, showURL
-    , runRouteT, Site(..), setDefault, mkSitePI)
-import Web.Routes.TH           (derivePathInfo)
 import Web.Routes.Happstack    (implSite)
-
-route :: Sitemap -> RouteT Sitemap (ServerPartT IO) Response
-route url =
-    case url of
-      Home                -> homePage
-      (User userId) -> userPage userId
-
-homePage :: RouteT Sitemap (ServerPartT IO) Response
-homePage = do
-  ok $ toResponse "bar"
-
-userPage :: UserId -> RouteT Sitemap (ServerPartT IO) Response
-userPage (UserId userId) = do
-  ok $ toResponse "foo"
-
-site :: Site Sitemap (ServerPartT IO Response)
-site =
-       setDefault Home $ mkSitePI (runRouteT route)
 
 main :: IO ()
 main = simpleHTTP nullConf $ msum
@@ -42,14 +19,3 @@ main = simpleHTTP nullConf $ msum
   , implSite (pack "http://localhost:8000") (pack "/route") site
   , seeOther "/r" (toResponse ())
   ]
-
--- handler :: Sitemap -> RouteT Sitemap IO ()
--- handler route = case route of
---   Index -> do
---     posts <- liftIO getPosts
---     liftIO $ putStrLn "Posts:"
---     forM_ posts $ \post -> do
---       postUrl <- showURL (Post (postId post))
---       liftIO $ putStrLn $
---         Text.unpack (postTitle post) ++ " - " ++ Text.unpack postUrl
-
---
+++ src/route/User.hs
@@ -0,0 +1,37 @@
+--Start-Module darf nicht anders als Main heißen
+module Route.User where
+
+import Domain.User
+
+import Prelude                 hiding (head)
+
+import Control.Monad           (msum)
+import Data.Data               (Data, Typeable)
+import Data.Monoid             (mconcat)
+import Data.Text
+import Happstack.Server
+    ( Response, ServerPartT, ok, toResponse, simpleHTTP
+    , nullConf, seeOther, dir, notFound, seeOther)
+import Web.Routes
+    ( PathInfo(..), RouteT, showURL
+    , runRouteT, Site(..), setDefault, mkSitePI)
+import Web.Routes.TH           (derivePathInfo)
+import Web.Routes.Happstack    (implSite)
+
+route :: Sitemap -> RouteT Sitemap (ServerPartT IO) Response
+route url =
+    case url of
+      Home                -> homePage
+      (User userId) -> userPage userId
+
+homePage :: RouteT Sitemap (ServerPartT IO) Response
+homePage = do
+  ok $ toResponse "bar"
+
+userPage :: UserId -> RouteT Sitemap (ServerPartT IO) Response
+userPage (UserId userId) = do
+  ok $ toResponse "foo"
+
+site :: Site Sitemap (ServerPartT IO Response)
+site =
+       setDefault Home $ mkSitePI (runRouteT route)
