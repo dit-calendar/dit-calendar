fa932406e564b6484ed967f8074f7e790ef556e7
Settembrini <a.befort@googlemail.com>
2017-01-11 21:33:12 +0100

    Init

--- library/AppStart.hs
+++ library/AppStart.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE TemplateHaskell #-}
 module AppStart where
 
 import Domain.User as Domain
+import Happstack.Foundation
+import System.FilePath      ((</>))
 import Route.Routing as Route
 
 import Prelude                 hiding ( head )
@@ -14,6 +17,8 @@ import Web.Routes.Happstack    ( implSite )
 import Control.Exception    ( bracket )
 import Data.Acid            ( openLocalState )
 import Data.Acid.Local      ( createCheckpointAndClose )
+import Data.Maybe           (fromMaybe)
+
 
 run :: IO ()
 run =
@@ -27,3 +32,18 @@ run =
                     , implSite (pack "http://localhost:8000") (pack "/route") $ Route.site acid
                     , seeOther "/r" (toResponse ())
                     ])
+
+
+data Acid = Acid
+   {
+   acidUserListState    :: AcidState UserList
+   }
+
+withAcid :: Maybe FilePath -- ^ state directory
+         -> (Acid -> IO a) -- ^ action
+         -> IO a
+withAcid mBasePath f =
+    let basePath = fromMaybe "_state" mBasePath 
+        countPath = basePath </> "userlist"
+    in bracket (openLocalStateFrom countPath  Domain.initialUserListState) (createCheckpointAndClose) $ \paste ->
+        f (Acid paste)
--- library/Controller/Repo.hs
+++ library/Controller/Repo.hs
@@ -29,27 +29,3 @@ data Acid = Acid
    {
    acidUserListState    :: AcidState UserList
    }
-
-withLocalState
-  :: ( MonadBaseControl IO m
-     , MonadIO m
-     , IsAcidic st
-     , Typeable st
-     ) =>
-     Maybe FilePath        -- ^ path to state directory
-  -> st                    -- ^ initial state value
-  -> (AcidState st -> m a) -- ^ function which uses the
-                           --   `AcidState` handle
-  -> m a
-withLocalState mPath initialState =
-  bracket (liftIO $ open initialState)
-          (liftIO . createCheckpointAndClose)
-  where
-    open = maybe openLocalState openLocalStateFrom mPath
-
-withAcid :: Maybe FilePath -> (Acid -> IO a) -> IO a
-withAcid mBasePath action =
-  let basePath = fromMaybe "_state" mBasePath
-      countPath = Just $ basePath </> "userlist"
-  in withLocalState countPath User.initialUserListState $ \c ->
-      action (Acid c)
