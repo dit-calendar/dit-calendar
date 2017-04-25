d7a234d752c58314cc55a68227c11ade25cb2b4f
Alex <a.befort@googlemail.com>
2017-04-25 21:23:20 +0200

    Add function for delete calendarEntries from user

--- library/Repository/UserRepo.hs
+++ library/Repository/UserRepo.hs
@@ -9,6 +9,7 @@ import Control.Monad.IO.Class
 import Domain.User              ( User(..) )
 import Domain.Types             ( UserId, EntryId )
 import Repository.Acid.UserAcid as UserAcid
+import Repository.Acid.CalendarAcid as CalendarAcid
 
 addCalendarEntryToUser :: (HasAcidState m UserAcid.UserList, MonadIO m) => 
     User -> EntryId -> m ()
@@ -16,3 +17,15 @@ addCalendarEntryToUser user entryId =
     let updatedUser = user {calendarEntrys = calendarEntrys user ++ [entryId]} in
         update $ UserAcid.UpdateUser updatedUser
 
+deleteUser :: (MonadIO m, HasAcidState m EntryList, HasAcidState m UserList) =>
+     User -> m ()
+deleteUser user =
+    let calendarToDelete = calendarEntrys user in
+        do
+            deleteCalendar calendarToDelete
+            update $ UserAcid.DeleteUser (userId user)
+
+deleteCalendar :: (HasAcidState m CalendarAcid.EntryList, MonadIO m) =>
+                   [EntryId] -> m ()
+deleteCalendar = foldr (\ x -> (>>) (update $ CalendarAcid.DeleteEntry x))
+        (return ())
