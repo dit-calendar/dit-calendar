3c652d3c000b29e3bb02ff5f8c3b1af687a52c6f
Viktor Befort <v.befort@googlemail.com>
2017-06-15 20:41:35 +0200

    add more tests to CalendarAcid

--- testsuite/Data/Repository/Acid/CalendarAcidSpec.hs
+++ testsuite/Data/Repository/Acid/CalendarAcidSpec.hs
@@ -2,7 +2,7 @@ module Data.Repository.Acid.CalendarAcidSpec (spec) where
 
 import Test.Hspec
 
-import Data.Maybe                 ( isJust, fromJust)
+import Data.Maybe                 ( isJust, fromJust, isNothing)
 import Data.Acid                  ( AcidState, openLocalState, closeAcidState, query, update )
 import Control.Exception          ( bracket )
 import Data.IxSet                 ( IxSet(..), insert, empty )
@@ -32,13 +32,6 @@ spec =
                   entryState `shouldSatisfy` isJust
                   fromJust entryState `shouldBe` CalendarEntry{ description="Foo", entryId=0, userId=0 }
 
-              it "all and check length" $
-                \c -> do
-                  entryList      <- query c GetEntryList
-                  let entryCount = nextEntryId entryList
-                  entryState     <- query c CalendarAcid.AllEntrys
-                  length entryState `shouldBe` entryCount
-
           describe "create" $ do
               it "new and check existence" $
                 \c -> do
@@ -56,3 +49,25 @@ spec =
                   entryList    <- query c GetEntryList
                   nextEntryId entryList `shouldBe` oldId + 1
 
+          describe "delete" $ do
+              it "create/delete Entry and check existence" $
+                \ c -> do 
+                  entryList <- query c GetEntryList
+                  _ <- update c (NewEntry "Zahnarzt" 1)
+                  entryState <- update c $ CalendarAcid.DeleteEntry $ nextEntryId entryList
+                  entryList <- query c GetEntryList
+                  entryState <- query c $ CalendarAcid.EntryById $ nextEntryId entryList
+                  entryState `shouldSatisfy` isNothing
+
+          describe "update" $ do
+              it "change attributes and check changes" $
+                \c -> do
+                  entryList   <- query c GetEntryList
+                  let eId = nextEntryId entryList
+                  _           <- update c (NewEntry "Termin 1" 2)
+                  entryState  <- query c $ CalendarAcid.EntryById $ eId
+                  let updatedEntry = (fromJust entryState) {description = "Termin 2"}
+                  _           <- update c $ CalendarAcid.UpdateEntry updatedEntry
+                  entryState  <- query c $ CalendarAcid.EntryById $ eId
+                  entryState `shouldSatisfy` isJust
+                  fromJust entryState `shouldBe` CalendarEntry{ description="Termin 2", entryId=eId, userId=2}
