4418623b4c3649363a96ac3bad409f198d68bf94
Alex <a.befort@googlemail.com>
2017-06-20 20:35:37 +0200

    Add controller for Task

---
+++ library/Controller/TaskController.hs
@@ -0,0 +1,49 @@
+module Controller.TaskController where
+
+import Happstack.Server         ( ok, toResponse, lookRead 
+                                , Method(GET), method)
+import Happstack.Foundation     ( query, update, delete )
+
+import Data.Domain.Task              as Task      ( Task(..))
+import Data.Domain.Types             ( TaskId, EntryId )
+import Data.Repository.Acid.TaskAcid as TaskAcid
+import Data.Repository.TaskRepo      as TaskRepo
+import Controller.AcidHelper    ( CtrlV )
+
+--handler for taskPage
+taskPage :: TaskId -> CtrlV
+taskPage i =
+    do
+       mTask <- query (TaskAcid.TaskById i)
+       case mTask of
+            Nothing ->
+                ok $ toResponse $ "Could not find a task with id " ++ show i
+            (Just u) ->
+                ok $ toResponse $ "peeked at the task and saw: " ++ show u
+
+createTask :: String -> CtrlV
+createTask description =
+    do
+        mTask <- update (TaskAcid.NewTask description)
+        ok $ toResponse $ "Task created: " ++ show mTask
+
+updateTask :: TaskId -> String -> CtrlV
+updateTask id description =
+    do
+       mTask <- query (TaskAcid.TaskById id)
+       case mTask of
+            Nothing ->
+                ok $ toResponse $ "Could not find a task with id " ++ show id
+            (Just u) -> do
+                 update (TaskAcid.NewTask description)
+                 ok $ toResponse $ "Task with id:" ++ show id ++ "updated"
+
+deleteTask :: TaskId -> CtrlV
+deleteTask i = do
+    mTask <- query (TaskAcid.TaskById i)
+    case mTask of
+        Nothing ->
+            ok $ toResponse $ "Could not find a task with id " ++ show i
+        (Just u) -> do
+            delete (TaskAcid.NewTask description)
+            ok $ toResponse $ "Task with id:" ++ show i ++ "deleted"
