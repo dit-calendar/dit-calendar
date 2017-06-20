694b80f03362661d7e02e49fc73b7cce1fac233c
Alex <a.befort@googlemail.com>
2017-06-20 21:15:33 +0200

    Add acid instance for task

--- library/Controller/AcidHelper.hs
+++ library/Controller/AcidHelper.hs
@@ -14,6 +14,7 @@ import Happstack.Foundation ( FoundationT, HasAcidState(..), FoundationT', getAc
 
 import Data.Repository.Acid.UserAcid          as UserAcid
 import Data.Repository.Acid.CalendarAcid      as CalendarAcid
+import Data.Repository.Acid.TaskAcid          as TaskAcid
 import Route.PageEnum       ( SiteMap )
 
 
@@ -24,6 +25,7 @@ data Acid = Acid
    {
      acidUserListState         :: AcidState UserAcid.UserList
      , acidEntryListState      :: AcidState CalendarAcid.EntryList
+     , acidTaskListState      :: AcidState TaskAcid.TaskList
    }
 
 instance (Functor m, Monad m) => HasAcidState (FoundationT' url Acid reqSt m) UserAcid.UserList where
@@ -32,6 +34,9 @@ instance (Functor m, Monad m) => HasAcidState (FoundationT' url Acid reqSt m) Us
 instance (Functor m, Monad m) => HasAcidState (FoundationT' url Acid reqSt m) CalendarAcid.EntryList where
     getAcidState = acidEntryListState <$> getAcidSt
 
+instance (Functor m, Monad m) => HasAcidState (FoundationT' url Acid reqSt m) TaskAcid.TaskList where
+    getAcidState = acidTaskListState <$> getAcidSt
+
 withAcid :: Maybe FilePath -- ^ state directory
          -> (Acid -> IO a) -- ^ action
          -> IO a
@@ -39,6 +44,8 @@ withAcid mBasePath f =
     let basePath = fromMaybe "state" mBasePath
         userPath = basePath </> "userList"
         calendarEntryPath = basePath </> "entryList"
+        taskPath = basePath </> "taskList"
     in bracket (openLocalStateFrom userPath UserAcid.initialUserListState) createCheckpointAndClose $ \c ->
        bracket (openLocalStateFrom calendarEntryPath CalendarAcid.initialEntryListState) createCheckpointAndClose $ \g ->
-        f (Acid c g)
+       bracket (openLocalStateFrom taskPath TaskAcid.initialTaskListState) createCheckpointAndClose $ \t ->
+        f (Acid c g t)
