40d29591596c851701700cf5ecc9a5eb2d4f2676
Alex <a.befort@googlemail.com>
2017-07-18 20:13:48 +0200

    Add and remove task from user:
    - Add list with task ids to user
    - extend addUserToTask and removeUserFromTask

--- library/Data/Domain/User.hs
+++ library/Data/Domain/User.hs
@@ -5,8 +5,8 @@ module Data.Domain.User where
 import Data.Data                ( Data, Typeable )
 import Data.SafeCopy            ( base, deriveSafeCopy )
 
-import Data.Domain.Types             ( UserId, EntryId )
+import Data.Domain.Types             ( UserId, EntryId, TaskId )
 
-data User = User { name :: String, userId :: UserId, calendarEntries :: [EntryId] }
+data User = User { name :: String, userId :: UserId, calendarEntries :: [EntryId], belongingTasks :: [TaskId] }
     deriving (Eq, Ord, Read, Show, Data, Typeable)
 $(deriveSafeCopy 0 'base ''User)
--- library/Data/Repository/TaskRepo.hs
+++ library/Data/Repository/TaskRepo.hs
@@ -2,15 +2,17 @@
 
 module Data.Repository.TaskRepo where
 
-import Happstack.Foundation       ( update, HasAcidState )
+import Happstack.Foundation       ( query, update, HasAcidState )
 import Control.Monad.IO.Class
 import Data.List                  ( delete )
 import Data.Maybe                 ( fromJust )
 
 import Data.Repository.Acid.TaskAcid          as TaskAcid
 import Data.Repository.Acid.CalendarAcid      as CalendarAcid
+import Data.Repository.Acid.UserAcid          as UserAcid
 import Data.Domain.CalendarEntry              as CalendarEntry
 import Data.Domain.Task                       as Task
+import Data.Domain.User                       as User
 import Data.Domain.Types          ( UserId, TaskId, EntryId )
 
 
@@ -35,17 +37,35 @@ updateTask task newDescription =
     let updatedTask = task {Task.description = newDescription} in
         update $ TaskAcid.UpdateTask updatedTask
 
-addUserToTask :: (HasAcidState m TaskAcid.TaskList, MonadIO m) =>
+addUserToTask :: (HasAcidState m TaskAcid.TaskList, HasAcidState m UserAcid.UserList, MonadIO m) =>
     Task -> UserId -> m ()
 addUserToTask task userId =
     let updatedTask = task {belongingUsers = belongingUsers task ++ [userId]} in
-        update $ TaskAcid.UpdateTask updatedTask
+        do
+            mUser <- query (UserAcid.UserById userId)
+            addTaskToUser (taskId task) (fromJust mUser)
+            update $ TaskAcid.UpdateTask updatedTask
 
-removeUserFromTask :: (HasAcidState m TaskAcid.TaskList, MonadIO m) =>
+addTaskToUser :: (HasAcidState m UserAcid.UserList, MonadIO m) =>
+    TaskId -> User -> m ()
+addTaskToUser taskId user =
+    let updatedUser = user {belongingTasks = belongingTasks user ++ [taskId]} in
+        update $ UserAcid.UpdateUser updatedUser
+
+removeUserFromTask :: (HasAcidState m TaskAcid.TaskList, HasAcidState m UserAcid.UserList, MonadIO m) =>
                       Task -> UserId -> m ()
 removeUserFromTask task userId =
     let updatedTask = task {belongingUsers = delete userId (belongingUsers task)} in
-        update $ TaskAcid.UpdateTask updatedTask
+        do
+            mUser <- query (UserAcid.UserById userId)
+            deleteTaskFromUser (taskId task) (fromJust mUser)
+            update $ TaskAcid.UpdateTask updatedTask
+
+deleteTaskFromUser :: (HasAcidState m UserAcid.UserList, MonadIO m) =>
+    TaskId -> User -> m ()
+deleteTaskFromUser taskId user =
+    let updatedUser = user {belongingTasks = delete taskId (belongingTasks user)} in
+        update $ UserAcid.UpdateUser updatedUser
 
 deleteTask :: (HasAcidState m TaskAcid.TaskList, MonadIO m) =>
                    [TaskId] -> m ()
