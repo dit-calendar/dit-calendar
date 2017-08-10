68658aa1ea209d2b76f6cd390537c74f9264326b
Viktor Befort <v.befort@googlemail.com>
2017-08-10 16:18:27 +0200

    repair broken code * add missing imports * delete not existing module from .cabal

--- library/Data/Repository/CalendarRepoHelper.hs
+++ library/Data/Repository/CalendarRepoHelper.hs
@@ -1,17 +1,19 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Data.Repository.CalendarRepoHelper where
 
-import Happstack.Foundation     ( HasAcidState )
+import Happstack.Foundation     ( HasAcidState, query )
 import Data.Domain.User                      as User
 import Data.Domain.CalendarEntry             as CalendarEntry
 import Data.Domain.Types        ( EntryId )
 import Control.Monad.IO.Class
+import Data.Maybe               ( fromJust )
 
 import qualified Data.Repository.UserRepo             as UserRepo
 import qualified Data.Repository.CalendarRepo         as CalendarRepo
-
+import qualified Data.Repository.CalendarTaskRepo     as CalendarTaskRepo
 import qualified Data.Repository.Acid.UserAcid        as UserAcid
 import qualified Data.Repository.Acid.CalendarAcid    as CalendarAcid
+import qualified Data.Repository.Acid.TaskAcid        as TaskAcid
 
 createEntry :: (HasAcidState m CalendarAcid.EntryList, HasAcidState m UserAcid.UserList,
             MonadIO m) => String -> User -> m CalendarEntry
