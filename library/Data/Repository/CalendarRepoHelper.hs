009ddb05a489abd3c7d9a50deab8d7349906f610
Viktor Befort <v.befort@googlemail.com>
2017-08-10 16:33:31 +0200

    move code from CalendarTaskRepo to CalendarRepoHelper

--- library/Data/Repository/CalendarRepoHelper.hs
+++ library/Data/Repository/CalendarRepoHelper.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
-module Data.Repository.CalendarRepoHelper where
+module Data.Repository.CalendarRepoHelper ( createEntry, removeCalendar ) where
 
 import Happstack.Foundation     ( HasAcidState, query )
 import Data.Domain.User                      as User
@@ -10,7 +10,7 @@ import Data.Maybe               ( fromJust )
 
 import qualified Data.Repository.UserRepo             as UserRepo
 import qualified Data.Repository.CalendarRepo         as CalendarRepo
-import qualified Data.Repository.CalendarTaskRepo     as CalendarTaskRepo
+import qualified Data.Repository.TaskRepo             as TaskRepo
 import qualified Data.Repository.Acid.UserAcid        as UserAcid
 import qualified Data.Repository.Acid.CalendarAcid    as CalendarAcid
 import qualified Data.Repository.Acid.TaskAcid        as TaskAcid
@@ -28,5 +28,14 @@ removeCalendar calendarEntry = let cEntryId = entryId calendarEntry in
     do
        mUser <- query (UserAcid.UserById (CalendarEntry.userId calendarEntry))
        UserRepo.deleteCalendarEntryFromUser (fromJust mUser) cEntryId
-       CalendarTaskRepo.deleteCalendarsTasks calendarEntry
+       deleteCalendarsTasks calendarEntry
        CalendarRepo.deleteCalendar [cEntryId]
+
+deleteCalendarsTasks :: (HasAcidState m UserAcid.UserList, HasAcidState m TaskAcid.TaskList, MonadIO m)
+    => CalendarEntry -> m ()
+deleteCalendarsTasks calendar =
+    foldr (\ x ->
+      (>>) (do
+        mTask <- query (TaskAcid.TaskById x)
+        TaskRepo.deleteTask (fromJust mTask) ))
+    (return ()) $ CalendarEntry.calendarTasks calendar
