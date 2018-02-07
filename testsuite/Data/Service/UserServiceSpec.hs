4f055637e700d188ab82e8006b785f466cf8cc8e
Alex <a.befort@googlemail.com>
2018-02-07 20:15:40 +0100

    Add test for UserService

---
+++ testsuite/Data/Service/UserServiceSpec.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Data.Service.UserServiceSpec (spec) where
+
+import Test.Hspec
+import Control.Monad.TestFixture
+import Control.Monad.TestFixture.TH
+
+import Control.Monad.IO.Class
+import Control.Monad.Identity        ( Identity )
+import Control.Monad.Writer.Class    ( tell )
+
+import Data.Domain.CalendarEntry  as CalendarEntry
+import Data.Domain.User           as User
+import Data.Domain.Task           as Task
+import Data.Domain.Types          ( UserId, EntryId, TaskId )
+
+import Data.Service.MonadDB.User          ( MonadDBUserService )
+import Data.Service.MonadDB.Task          ( MonadDBTaskService )
+import Data.Service.MonadDB.Calendar      ( MonadDBCalendarService )
+
+import qualified Data.Service.User          as UserService
+
+
+mkFixture "Fixture" [ts| MonadDBUserService, MonadDBTaskService, MonadDBCalendarService |]
+
+userFromDb = User{ name="Foo", User.userId=10, calendarEntries=[], belongingTasks=[1,2,3] }
+taskFromDb = Task{ Task.description="task1", taskId=1, belongingUsers=[]}
+
+fixture = Fixture { _newCalendarEntry = undefined
+                  , _deleteCalendarEntry = \(a) -> tell (show a)
+                  , _deleteTaskFromCalendarEntry = undefined
+                  , _addTaskToCalendarEntry = undefined
+                  , _updateTask = undefined
+                  , _deleteTask = undefined
+                  , _createTask = undefined
+                  , _getTask = undefined
+                  , _createUser = undefined
+                  , _deleteUser = \(a) -> tell (show a)
+                  , _updateName = undefined
+                  , _addCalendarEntryToUser = undefined
+                  , _deleteCalendarEntryFromUser = undefined
+                  , _addTaskToUser = undefined
+                  , _deleteTaskFromUser = undefined
+                  , _getUser = undefined
+                  }
+
+instance MonadIO Identity where
+    liftIO = undefined
+
+
+spec = describe "UserService" $ do
+    it "deleteUser" $ do
+        let user = User{ name="Foo", User.userId=10, calendarEntries=[1,2], belongingTasks=[] }
+        let (_, log) = evalTestFixture (UserService.deleteUser user) fixture
+        log `shouldBe` ("12" ++ show user)
