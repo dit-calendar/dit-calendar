aae6a7f934e83f8dfe7bab8531a010e935ad98d8
alex <a.befort@googlemail.com>
2019-02-26 21:06:44 +0100

    init

--- dit-calendar-client/src/Main.elm
+++ dit-calendar-client/src/Main.elm
@@ -1,5 +1,57 @@
-module Main exposing (main)
+module Main exposing (Model, Msg(..), emptyModel, init, main, update, view)
 
-import Html exposing (text)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
-main = text "hi"
+
+main : Program (Maybe Model) Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+type alias Model =
+    { quote : String
+    }
+
+
+emptyModel : Model
+emptyModel =
+    { quote = "" }
+
+
+init : Maybe Model -> ( Model, Cmd Msg )
+init maybeModel =
+    ( Maybe.withDefault emptyModel maybeModel, Cmd.none )
+
+
+type Msg
+    = GetQuote
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GetQuote ->
+            ( { model | quote = model.quote ++ "A quote! " }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ h2 [ class "text-center" ] [ text "Chuck Norris Quotes" ]
+        , p [ class "text-center" ]
+            [ button [ class "btn btn-success", onClick GetQuote ] [ text "Grab a quote!" ]
+            ]
+
+        -- Blockquote with quote
+        , blockquote []
+            [ p [] [ text model.quote ]
+            ]
+        ]
