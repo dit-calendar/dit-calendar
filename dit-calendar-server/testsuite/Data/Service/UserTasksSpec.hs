dcbfdd1b79971868bbe1f89ef45034aa115b5c1c
Viktor Befort <v.befort@googlemail.com>
2019-11-29 11:14:08 +0100

    refactor unit test

--- dit-calendar-server/testsuite/Data/Service/UserTasksSpec.hs
+++ dit-calendar-server/testsuite/Data/Service/UserTasksSpec.hs
@@ -12,6 +12,7 @@ import           Control.Monad.TestFixture
 import           Control.Monad.TestFixture.TH
 import           Data.Default                 (def)
 import           Test.Hspec
+import           Test.HUnit.Base              (assertEqual)
 
 import           Control.Monad.Identity       (Identity)
 import           Control.Monad.IO.Class
@@ -36,8 +37,8 @@ taskFromDb2 = def{ Task.description="task2", taskId=6, startTime=Nothing, endTim
 fixture :: (Monad m, MonadWriter [String] m) => Fixture m
 fixture = Fixture {  _updateTask = \a -> tell [show a] >>= (\_ -> return $ Right a)
                   , _findTaskById = \a -> return $ Just (if a == 1 then taskFromDb else taskFromDb2)
-                  , _addTaskToUser = \user taskId -> tell [show user] >> tell [show taskId] >>= (\_ -> return $ Right user)
-                  , _deleteTaskFromUser = \x a -> tell [show x] >> tell [show a] >>= (\_ -> return $ Right x)
+                  , _addTaskToUser = \user taskId -> tell [show user ++ show taskId] >>= (\_ -> return $ Right user)
+                  , _deleteTaskFromUser = \x a -> tell [show x ++ show a] >>= (\_ -> return $ Right x)
                   , _findUserById = \a -> return $ Just (if a == 7 then userFromDb else userFromDb2)
                   }
 
@@ -49,34 +50,28 @@ spec = describe "UserTasksSpec" $ do
     it "deleteTaskFromAllUsers" $ do
         let task = def{ Task.description="task1", taskId=1, assignedUsers=[7,8], startTime=Nothing, endTime=Nothing}
         let (_, log) = evalTestFixture (UserTasksService.deleteTaskFromAllUsersImpl task) fixture
-        length log `shouldBe` 4
+        length log `shouldBe` 2
         -- deleteTaskFromUser calls
-        log!!0 `shouldBe` show userFromDb
-        log!!1 `shouldBe` show task
-        log!!2 `shouldBe` show userFromDb2
-        log!!3 `shouldBe` show task
+        assertEqual "deleteTaskFromUser callend with wrong user or task" (log!!0) (show userFromDb ++ show task)
+        assertEqual "deleteTaskFromUser callend with wrong user or task" (log!!1) (show userFromDb2 ++ show task)
     it "addUserToTask" $ do
         let task = def { Task.description="task1", taskId=1, assignedUsers=[2], startTime=Nothing, endTime=Nothing}
         let expectedTask = def { Task.description="task1", taskId=1, assignedUsers=[10, 2], startTime=Nothing, endTime=Nothing}
         let (_, log) = evalTestFixture (UserTasksService.addUserToTaskImpl task userFromDb) fixture
-        length log `shouldBe` 3
-        log!!0 `shouldBe` show userFromDb
-        log!!1 `shouldBe` show (Task.taskId task)
-        log!!2 `shouldBe` show expectedTask
+        length log `shouldBe` 2
+        assertEqual "addTaskToUser callend with wrong user or task" (log!!0) (show userFromDb ++ show (Task.taskId task))
+        assertEqual "task update with new user failed" (log!!1) (show expectedTask)
     it "removeUserFromTask" $ do
         let task = def { Task.description="task1", taskId=1, assignedUsers=[2,10], startTime=Nothing, endTime=Nothing}
         let expectedTask = def { Task.description="task1", taskId=1, assignedUsers=[2], startTime=Nothing, endTime=Nothing}
         let (_, log) = evalTestFixture (UserTasksService.removeUserFromTaskImpl task userFromDb) fixture
-        length log `shouldBe` 3
-        log!!0 `shouldBe` show userFromDb
-        log!!1 `shouldBe` show task
-        log!!2 `shouldBe` show expectedTask
+        length log `shouldBe` 2
+        assertEqual "deleteTaskFromUser callend with wrong user or task" (log!!0) (show userFromDb ++ show task)
+        assertEqual "update task without old user" (log!!01) (show expectedTask)
     it "removeUserFromTasks" $ do
         let (_, log) = evalTestFixture (UserTasksService.removeUserFromTasksImpl userFromDb2) fixture
-        length log `shouldBe` 6
-        log!!0 `shouldBe` show userFromDb2
-        log!!1 `shouldBe` show taskFromDb
-        log!!2 `shouldBe` show taskFromDb {assignedUsers=[11]}
-        log!!3 `shouldBe` show userFromDb2
-        log!!4 `shouldBe` show taskFromDb2
-        log!!5 `shouldBe` show taskFromDb2 {assignedUsers=[]}
+        length log `shouldBe` 4
+        assertEqual "deleteTaskFromUser callend with wrong user or task" (log!!0) (show userFromDb2 ++ show taskFromDb)
+        assertEqual "update task without old user" (log!!1) (show taskFromDb {assignedUsers=[11]})
+        assertEqual "deleteTaskFromUser callend with wrong user or task" (log!!2) (show userFromDb2 ++ show taskFromDb2)
+        assertEqual "update task without old user" (log!!3) (show taskFromDb2 {assignedUsers=[]})
