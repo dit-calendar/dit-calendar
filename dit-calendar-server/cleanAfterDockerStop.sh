b9696d09ce55ee801e632ab54b5660f84b7741e0
Viktor Befort <v.befort@googlemail.com>
2019-12-27 10:40:34 +0100

    remove script for cleaning acid locks, because not needed

--- dit-calendar-server/cleanAfterDockerStop.sh
+++
@@ -1,2 +0,0 @@
-sudo find state/ -user root -exec sudo chown $USER: {} +
-rm state/authenticate/core/open.lock state/authenticate/password/open.lock state/entryList/open.lock state/taskList/open.lock state/userList/open.lock
