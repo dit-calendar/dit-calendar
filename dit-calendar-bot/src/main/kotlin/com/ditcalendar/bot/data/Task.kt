d7ed4978f9766335cd3e3cfd1e9c5162aee4b8db
alex <a.befort@googlemail.com>
2020-03-27 13:23:43 +0100

    make botName configurable

--- dit-calendar-bot/src/main/kotlin/com/ditcalendar/bot/data/Task.kt
+++ dit-calendar-bot/src/main/kotlin/com/ditcalendar/bot/data/Task.kt
@@ -1,5 +1,7 @@
 package com.ditcalendar.bot.data
 
+import com.ditcalendar.bot.config.bot_name
+import com.ditcalendar.bot.config.config
 import com.ditcalendar.bot.data.core.Base
 import com.ditcalendar.bot.data.core.DateSerializer
 import kotlinx.serialization.Serializable
@@ -15,15 +17,18 @@ data class Task(val taskId: Long,
                 @Serializable(with = DateSerializer::class)
                 val endTime: Date? = null) : Base() {
 
+    @Transient
+    private val botName = config().value[bot_name]
+
     override fun toStringInMarkdown(): String =
             """_Task_: $description
                _Datum_: $startTime
-               [unassign me](https://t.me/ditcalendarbot?start=unassign_$taskId)
+               [unassign me](https://t.me/$botName?start=unassign_$taskId)
             """.trimIndent()
 
     fun toStringForCalendarInMarkdown(): String =
             """
-                _Task_: $description [assign me](https://t.me/ditcalendarbot?start=assign_$taskId)
+                _Task_: $description [assign me](https://t.me/$botName?start=assign_$taskId)
                 _Datum_: $startTime
             """.trimIndent()
 
